9a97f5a5021dd117e9d68f872b68e6d4730aade1
Kalin Karolev <karolevkalin@gmail.com>
2025-03-01 14:47:30 +0200

    SparkFund Application #1 (Initial commit) - App configuration - First HTML and CSS files - Index and Spark controller

---
+++ SparkFund/src/main/java/app/web/IndexController.java
@@ -0,0 +1,30 @@
+package app.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class IndexController {
+
+    @GetMapping
+    public String getIndexPage() {
+        return "index";
+    }
+
+    @GetMapping("/register")
+    public String getRegisterPage() {
+        return "register";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "login";
+    }
+
+    @GetMapping("/home")
+    public String getHomePage() {
+        return "home";
+    }
+}
---
+++ SparkFund/src/test/java/com/example/SparkFund/SparkFundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SparkFund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkFundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
